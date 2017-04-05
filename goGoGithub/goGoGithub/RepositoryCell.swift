64254da0b37b424dad161e6e7f9292ea2cffd6de
Regenal Grant <regenal@mac.com>
2017-04-05 16:50:44 -0700

    Create RepoDetailViewController that will display Repository information. Implement the dismissal of the RepoDetailViewController.

--- goGoGithub/goGoGithub/RepositoryCell.swift
+++ goGoGithub/goGoGithub/RepositoryCell.swift
@@ -20,12 +20,10 @@ class RepositoryCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
     
